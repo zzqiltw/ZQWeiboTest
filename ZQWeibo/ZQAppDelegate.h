d2c13696efae1968105f7c44296e9ff1edb25201
飞凫 <zhiqin.zzq@alibaba-inc.com>
2015-01-06 15:26:13 +0800

    Initial Commit

---
+++ ZQWeibo/ZQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZQAppDelegate.h
+//  ZQWeibo
+//
+//  Created by zzqiltw on 15-1-6.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZQWeibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZQWeibo
+//
+//  Created by zzqiltw on 15-1-6.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZQAppDelegate class]));
+    }
+}
