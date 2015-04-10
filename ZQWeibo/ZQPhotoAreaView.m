591e89c04e0d3f0c966acd2fe6e86541a3a126e5
飞凫 <zhiqin.zzq@alibaba-inc.com>
2015-04-10 22:41:46 +0800

    自己写的图片放大缩小的，细节功能缺比较多。

--- ZQWeibo/ZQPhotoAreaView.m
+++ ZQWeibo/ZQPhotoAreaView.m
@@ -13,6 +13,12 @@
 #import "MJPhotoBrowser/MJPhotoBrowser.h"
 #import "MJPhoto.h"
 
+@interface ZQPhotoAreaView ()
+
+@property (nonatomic, weak) ZQPhotoView *currentNewPhotoView;
+@property (nonatomic, assign) CGRect currentFrame;
+
+@end
 
 @implementation ZQPhotoAreaView
 
@@ -35,31 +41,63 @@
 
 - (void)photoTap:(UIGestureRecognizer *)recognizer
 {
-    int count = self.photos.count;
+    UIButton *cover = [[UIButton alloc] init];
+    cover.backgroundColor = [UIColor blackColor];
+    cover.alpha = 0.9;
+    [cover addTarget:self action:@selector(coverTap:) forControlEvents:UIControlEventTouchUpInside];
+    cover.frame = [UIScreen mainScreen].applicationFrame;
+    [[UIApplication sharedApplication].keyWindow addSubview:cover];
     
-    // MJ老师三方框架:图片浏览器
-    
-    // 1.封装图片数据
-    NSMutableArray *myphotos = [NSMutableArray arrayWithCapacity:count];
-    for (int i = 0; i<count; i++) {
-        // 一个MJPhoto对应一张显示的图片
-        MJPhoto *mjPhoto = [[MJPhoto alloc] init];
-        
-        mjPhoto.srcImageView = self.subviews[i]; // 来源于哪个UIImageView
-        
-        ZQPhoto *zqPhoto = self.photos[i];
-        NSString *photoUrl = [zqPhoto.thumbnail_pic stringByReplacingOccurrencesOfString:@"thumbnail" withString:@"bmiddle"];
-        mjPhoto.url = [NSURL URLWithString:photoUrl]; // 图片路径
-        
-        [myphotos addObject:mjPhoto];
-    }
-    
-    // 2.显示相册
-    MJPhotoBrowser *browser = [[MJPhotoBrowser alloc] init];
-    browser.currentPhotoIndex = recognizer.view.tag; // 弹出相册时显示的第一张图片是？
-    browser.photos = myphotos; // 设置所有的图片
-    [browser show];
+    ZQPhotoView *currentPhotoView = (ZQPhotoView *)recognizer.view;
+    ZQPhotoView *newPhotoView = [[ZQPhotoView alloc] init];
+    newPhotoView.image = currentPhotoView.image;
+    newPhotoView.frame = [self convertRect:currentPhotoView.frame toView:cover];
+    self.currentFrame = newPhotoView.frame;
+    self.currentNewPhotoView = newPhotoView;
 
+    [UIView animateWithDuration:0.2f animations:^{
+        CGFloat x = 0;
+        CGFloat width = cover.frame.size.width;
+        CGFloat height = newPhotoView.frame.size.width * cover.frame.size.height / width;
+        CGFloat y = (cover.frame.size.height - height) * 0.5;
+        newPhotoView.frame = CGRectMake(x, y, width, height);
+        [cover addSubview:newPhotoView];
+    }];
+    
+//    int count = self.photos.count;
+//    // MJ老师三方框架:图片浏览器
+//    
+//    // 1.封装图片数据
+//    NSMutableArray *myphotos = [NSMutableArray arrayWithCapacity:count];
+//    for (int i = 0; i<count; i++) {
+//        // 一个MJPhoto对应一张显示的图片
+//        MJPhoto *mjPhoto = [[MJPhoto alloc] init];
+//        
+//        mjPhoto.srcImageView = self.subviews[i]; // 来源于哪个UIImageView
+//        
+//        ZQPhoto *zqPhoto = self.photos[i];
+//        NSString *photoUrl = [zqPhoto.thumbnail_pic stringByReplacingOccurrencesOfString:@"thumbnail" withString:@"bmiddle"];
+//        mjPhoto.url = [NSURL URLWithString:photoUrl]; // 图片路径
+//        
+//        [myphotos addObject:mjPhoto];
+//    }
+//    
+//    // 2.显示相册
+//    MJPhotoBrowser *browser = [[MJPhotoBrowser alloc] init];
+//    browser.currentPhotoIndex = recognizer.view.tag; // 弹出相册时显示的第一张图片是？
+//    browser.photos = myphotos; // 设置所有的图片
+//    [browser show];
+
+}
+
+- (void)coverTap:(UIButton *)cover
+{
+    [UIView animateWithDuration:0.2f animations:^{
+        cover.backgroundColor = [UIColor clearColor];
+        self.currentNewPhotoView.frame = self.currentFrame;
+    } completion:^(BOOL finished) {
+        [cover removeFromSuperview];
+    }];
 }
 
 - (void)setPhotos:(NSArray *)photos
